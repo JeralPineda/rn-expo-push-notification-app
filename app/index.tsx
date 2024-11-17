3852e6a8ac436e98a3a5c0b4a3c9f7e1dcb3240d
Jeral Pineda <jeral.cris@gmail.com>
2024-11-17 17:15:57 -0600

    fix: Inicio de proyecto - PushApp

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { Text, View } from "react-native";
+
+export default function PushApp() {
+  return (
+    <View>
+      <ThemedText>Hola mundo</ThemedText>
+    </View>
+  );
+}
